e4f05b6fb3934e220d2f5f10621ca19c1e866dc5
me <b.kooshan69@gmail.com>
2022-10-02 16:54:56 +0330

    modif\y

--- src/scrappy.montro.ts
+++ src/scrappy.montro.ts
@@ -13,13 +13,28 @@ const DEALERSHIP_URL = "https://www.monteroauto.com/vehicles";
 const initialiseScrappy = async (): Promise<void> => {
   const browser = await puppeteer.launch({
     headless: false,
-  }); 
+  });
+
+  const hrefs = [];
 
   const page: Page = await browser.newPage();
 
-  await page.goto(DEALERSHIP_URL, { waitUntil: "domcontentloaded" , timeout: 0});
+  await page.goto(DEALERSHIP_URL, {
+    waitUntil: "domcontentloaded",
+    timeout: 0,
+  });
 
-  await scrollPageToBottom(page, { size: 500, delay:500 })
+  for (let i = 0; i <= 6; i++)
+    await scrollPageToBottom(page, { size: 500});
 
-}
+  for (let i = 1; i <= 107; i++) {
+    const links = await page?.$$eval(
+      `body > main > div > div > div.wrapper > div.row > div.col.primary-col > div.row > div:nth-child(${i}) > div > div.vehicle-card__full-details > div > div.price-block > a`,
+      (element: any) => element.map((el: any) => el?.getAttribute("href"))[0]
+    );
+    hrefs.push(links);
+  }
+  console.log(hrefs);
+  console.log(hrefs.length);
+};
 (async () => await initialiseScrappy())();
