3e5081e2d7a18f011c74928d8e609d3b4429c623
me <b.kooshan69@gmail.com>
2022-10-15 16:55:58 +0330

    modif\y

--- src/information.ts
+++ src/information.ts
@@ -1,6 +1,5 @@
 export const credentials = {
   V12_LOGIN_URL: "https://www.v12software.com/accounts_v2/signup2/login",
-  // AUTOBUNNY_LOGIN_URL: "https://dealers.autobunny.ca/login",
   AUTOBUNNY_LOGIN_URL:"https://dealers.autobunny.ca/client/inventory"
 };
 export const providers = ["cargurus","dealerplus","zop","v12","autobunny","carpages"]
